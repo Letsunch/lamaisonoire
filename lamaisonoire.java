e48e0a1fdf17a395bb2c0f7b55fc5cfb62f4a46c
letsunch <calebdiba@gmail.com>
2025-09-27 15:28:12 +0200

    LAST PUSHHHHHHHHHHHHHHH

--- lamaisonoire.java
+++ lamaisonoire.java
@@ -25,20 +25,47 @@ public class lanaisonoire extends Bot {
         setRadarColor(Color.PURPLE);
         setScanColor(Color.PURPLE);
 
-        // Repeat while the bot is running
+        // Repeat while the bot is running - Gray bot style movement
         while (isRunning()) {
-            // Tell the game that when we take move, we'll also want to turn right... a lot
-            setTurnRight(20_000);
-            // Limit our speed to 5
-            setMaxSpeed(10);
-            // Start moving (and turning)
-            forward(8_000);
+            // Move in circles like Gray bot - turn right a lot
+            setTurnRight(10_000);
+            // Set speed similar to Gray bot
+            setMaxSpeed(6);
+            // Start moving and turning in circles
+            forward(10_000);
         }
     }
 
-    // We scanned another bot -> fire hard!
+    // Simple enemy detection and firing like Gray bot
     @Override
     public void onScannedBot(ScannedBotEvent e) {
+        // Fire maximum power like Gray bot - simple and effective
         fire(5);
+    }
+    
+    // Wall avoidance - turn away when hitting walls
+    @Override
+    public void onHitWall(HitWallEvent e) {
+        // Turn away from wall and move back
+        back(50);
+        turnRight(90);
+    }
+    
+    // Bot collision handling similar to Gray bot
+    @Override
+    public void onHitBot(HitBotEvent e) {
+        // Gray bot style collision handling
+        var direction = directionTo(e.getX(), e.getY());
+        var bearing = calcBearing(direction);
+        
+        // Fire if enemy is in front
+        if (bearing > -10 && bearing < 10) {
+            fire(5);
+        }
+        
+        // Turn to continue spinning if we rammed
+        if (e.isRammed()) {
+            turnRight(10);
+        }
     }
 }
